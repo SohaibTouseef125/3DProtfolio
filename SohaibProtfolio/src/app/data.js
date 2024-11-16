a00525e66b21f8e0447ee6b2e7c387a2f1433481
Sohaib Touseef <muhammadsohaib2233344@gmail.com>
2024-11-16 19:04:48 +0500

    3D Protfolio

---
+++ SohaibProtfolio/src/app/data.js
@@ -0,0 +1,178 @@
+/*
+Websites:
+
+- https://github.com/pmndrs/gltfjsx (GLTF JSX for 3D Models)
+- https://lucide.dev/icons/ (Lucide Icons)
+- https://github.com/anuraghazra/github-readme-stats (Github Readme Stats)
+- https://skillicons.dev (Skill Icons to show skills)
+- https://github-readme-streak-stats.herokuapp.com (Github Readme Streak Stats)
+
+:root {
+  --background: 27 27 27;
+  --foreground: 225 225 225;
+  --muted: 115 115 115;
+  --accent: 254 254 91; #FEFE5B
+}
+
+*/
+
+export const projectsData = [
+  {
+    id: 1,
+    name: "Resume-Builder",
+    description: "This ia a Resume-Builder",
+    date: "20224-07-15",
+    demoLink: "https://hackthons-project-resume-builder-oveg.vercel.app/",
+  },
+  {
+    id: 2,
+    name: "Dynamic Calculator",
+    description: "This is a Calculator",
+    date: "2022-04-18",
+    demoLink: "https://multiple-projects-dynamic.vercel.app/",
+  },
+  {
+    id: 3,
+    name: "Dynamic Blub",
+    description: "This is a Dynamic Blub",
+    date: "2022-02-14",
+    demoLink: "https://multiple-projects-dynamic-nah6.vercel.app/",
+  },
+  {
+    id: 4,
+    name: "BackGround Color Changer",
+    description: "This is a BackGround Color Changer",
+    date: "2022-11-05",
+    demoLink: "https://multiple-projects-dynamic-s7kn.vercel.app/",
+  },
+  {
+    id: 5,
+    name: "Amazon",
+    description: "This is a Amazon web clone",
+    date: "2022-02-14",
+    demoLink: "https://html-css-projects-pi-ecru.vercel.app/",
+  },
+  {
+    id: 6,
+    name: "Nurpur Web",
+    description: "This is Nurpur Web ",
+    date: "2022-09-10",
+    demoLink: "https://nurpur-web-clone.vercel.app/",
+  },
+  {
+    id: 7,
+    name: "The Straangers",
+    description: "This is a Simple web clone",
+    date: "2023-06-20",
+    demoLink: "https://html-css-projects-gbs9ej6dd-sohaib-touseefs-projects.vercel.app/",
+  },
+ 
+  {
+    id: 8,
+    name: "bootstrap Web",
+    description: "This is bootstrap Web Clone",
+    date: "2022-05-30",
+    demoLink: "https://bootstrap-web-lac.vercel.app/",
+  },
+  {
+    id: 0,
+    name: "envatomarket web",
+    description: "This is a envatomarket web clone",
+    date: "2022-02-14",
+    demoLink: "https://html-css-projects-theta.vercel.app/",
+  },
+  {
+    id: 10,
+    name: "Totoya Web ",
+    description: " This is a Totoya web clone",
+    date: "2022-07-12",
+    demoLink: "https://totoya-web-clone.vercel.app/",
+  },
+  {
+    id: 11,
+    name: "Simple Web Clone",
+    description: "This is a Simple Web Clone ",
+    date: "2022-10-01",
+    demoLink: "https://html-css-projects-luhs.vercel.app/",
+  },
+
+
+  {
+    id: 12,
+    name: "Simple Protfolio",
+    description: "Thsi is a Simple Protfolio",
+    date: "2022-03-22",
+    demoLink: "https://multiple-projects-dynamic-3flq.vercel.app/",
+  },
+ 
+  {
+    id: 13,
+    name: "Subscribe Button Sound",
+    description: "This is a Subscribe Button Sound",
+    date: "2022-02-14",
+    demoLink: "https://multiple-projects-dynamic-doki.vercel.app/",
+  },
+  {
+    id: 14,
+    name: "Dark-mode Light-mode",
+    description: "This is a Dark-mode Light-mode Background",
+    date: "2022-02-14",
+    demoLink: "https://multiple-projects-dynamic-pu84.vercel.app/",
+  },
+  {
+    id: 13,
+    name: "Silder",
+    description: "This is a Silder",
+    date: "2022-02-14",
+    demoLink: "https://multiple-projects-dynamic-mso6.vercel.app/",
+  },
+  {
+    id: 15,
+    name: "Resume-Builder",
+    description: "This is a Resume builder",
+    date: "2022-02-14",
+    demoLink: "https://hackthons-project-resume-builder.vercel.app/",
+  },
+
+ 
+];
+
+export const BtnList = [
+  { label: "Home", link: "/", icon: "home", newTab: false },
+  { label: "About", link: "/about", icon: "about", newTab: false },
+  { label: "Projects", link: "/projects", icon: "projects", newTab: false },
+  { label: "Contact", link: "/contact", icon: "contact", newTab: false },
+  {
+    label: "Github",
+    link: "https://github.com/SohaibTouseef125",
+    icon: "github",
+    newTab: true,
+  },
+  {
+    label: "LinkedIn",
+    link: "https://www.linkedin.com/in/sohaib-touseef-12a7b628a/",
+    icon: "linkedin",
+    newTab: true,
+  },
+  {
+    label: "Facebook",
+    link: "https://www.facebook.com/SohaibTouseef",
+    icon: "facebook",
+    newTab: true,
+  },
+  {
+    label: "Instagram",
+    link: "https://www.instagram.com/",
+    icon: "instagram",
+    newTab: true,
+  },
+
+  {
+    label: "Resume",
+    link: "/resume.pdf",
+    icon: "resume",
+    newTab: true,
+  },
+];
+
+
